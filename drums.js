f7bd39550ebe1671fe118436863d0f11cab07522
abhilashak2k <abhilashak2k@gmail.com>
2019-11-12 20:20:25 +0530

    detecting the key

--- drums.js
+++ drums.js
@@ -2,6 +2,9 @@
 
 window.addEventListener('keydown', function(e){
 
-	console.log("yep");//it enters this function yayy!!ss
+	console.log("yep");//it enters this function yayy!!
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //detect if there is a corresponding action and select it
+	console.log(audio);//yep it detects it!!
+
 
 });
