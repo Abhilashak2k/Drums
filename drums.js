4508c4ca07fffd6ff563f5f89012b1e192990fbb
abhilashak2k <abhilashak2k@gmail.com>
2019-11-12 20:50:51 +0530

    removing animation at button up

--- drums.js
+++ drums.js
@@ -4,7 +4,22 @@ window.addEventListener('keydown', function(e){
 
 	console.log("yep");//it enters this function yayy!!
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //detect if there is a corresponding action and select it
+	const animate = document.querySelector(`.key[data-key="${e.keyCode}"]`); //select using class key
+
 	console.log(audio);//yep it detects it!!
+	if(audio==null) {console.log("oops!"); return;}
+	audio.currentTime = 0; //even if i press 1 while play() is running, it should reset and start over
+	audio.play();
+
+	//animation part
+
+	animate.classList.add('playing'); //earlier playing was not included in the key div earier...
+});
 
 
+//removing animation part
+
+window.addEventListener('keyup', function (e){
+	const animate = document.querySelector(`.key[data-key="${e.keyCode}"]`); //select using class key
+	animate.classList.remove('playing');
 });
